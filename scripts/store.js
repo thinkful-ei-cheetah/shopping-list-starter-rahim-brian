eea133231d239a91b2424fbca73ee99f192e2991
rahmor <rahmor@yahoo.com>
2019-04-01 11:28:59 -0400

    fix errors

--- scripts/store.js
+++ scripts/store.js
@@ -1,13 +1,13 @@
 const store = (function() {
   const foo = "bar";
-  items: [
+  const items = [
     { id: cuid(), name: "apples", checked: false },
     { id: cuid(), name: "oranges", checked: false },
     { id: cuid(), name: "milk", checked: true },
     { id: cuid(), name: "bread", checked: false }
   ];
-  hideCheckedItems: false;
-  searchTerm: "";
+  const hideCheckedItems = false;
+  const searchTerm = "";
   return {
     items,
     hideCheckedItems,
